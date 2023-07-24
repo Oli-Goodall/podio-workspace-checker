f74f27697ab2d80f627d503e2c448853ada84ad9
Oli-Goodall <oligoodall96@gmail.com>
2023-07-24 17:57:03 +0100

    Starting to test podio API

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from tkinter import *
+from pypodio2 import api
+
+client_id = "olis-podio-app"
+client_secret = "BPuU6ZWR8uQP3HFMrU9HjfO1eEaChtJnDxoYosDEi8htMxakvzeVahCDnxzoXdm3"
+username = "oliver@theaccountancyfranchise.co.uk"
+password = "Oliver1234."
+
+c = api.OAuthClient(
+    client_id,
+    client_secret,
+    username,
+    password,    
+)
 
 main = Tk()
 
