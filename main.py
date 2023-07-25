1ec31a10382ec0d2fdec2449c66abe138f9e46c2
Oli-Goodall <oligoodall96@gmail.com>
2023-07-25 09:29:52 +0100

    Set up GUI as a class

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from tkinter import *
+import tkinter as tk
+import sys
+sys.path.append('./src/pypodio2/')
 from pypodio2 import api
 
-client_id = "olis-podio-app"
-client_secret = "BPuU6ZWR8uQP3HFMrU9HjfO1eEaChtJnDxoYosDEi8htMxakvzeVahCDnxzoXdm3"
-username = "oliver@theaccountancyfranchise.co.uk"
-password = "Oliver1234."
+class MyGUI:
 
-c = api.OAuthClient(
-    client_id,
-    client_secret,
-    username,
-    password,    
-)
+    def __init__(self):
 
-main = Tk()
+        self.client_id = "olis-podio-app"
+        self.client_secret = "BPuU6ZWR8uQP3HFMrU9HjfO1eEaChtJnDxoYosDEi8htMxakvzeVahCDnxzoXdm3"
+        self.username = "oliver@theaccountancyfranchise.co.uk"
+        self.password = "Oliver1234."
 
-main.mainloop()
+        self.c = api.OAuthClient(
+            self.client_id,
+            self.client_secret,
+            self.username,
+            self.password,    
+        )
+
+        
+
+        self.root = tk.Tk()
+
+        self.label = tk.Label(self.root, text="Your Message", font=('Arial, 18'))
+        self.label.pack(padx=10, pady=10)
+
+        self.textbox = tk.Text(self.root, height=5, font=('Arial', 16))
+        self.textbox.pack(padx=10, pady=10)
+
+        self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=('Arial', 16))
+
+        self.root.mainloop()
+
+MyGUI()
+
+
+
