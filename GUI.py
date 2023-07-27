1df0d2e4c23d4542d376d0f18c751a3d5167c554
Oli-Goodall <oligoodall96@gmail.com>
2023-07-27 11:07:50 +0100

    Moved api authentication to backendProcesses file

--- GUI.py
+++ GUI.py
@@ -1,25 +1,9 @@
 import tkinter as tk
-import sys
-sys.path.append('./src/pypodio2/')
-from pypodio2 import api
+from backendProcesses import c
 
 class WorkspaceCheckerGUI:
 
     def __init__(self):
-
-        #Credentials for Podio API authentication
-        self.client_id = "olis-podio-app"
-        self.client_secret = "BPuU6ZWR8uQP3HFMrU9HjfO1eEaChtJnDxoYosDEi8htMxakvzeVahCDnxzoXdm3"
-        self.username = "oliver@theaccountancyfranchise.co.uk"
-        self.password = "Oliver1234."
-
-        #Authenticaiton with above credentials
-        self.c = api.OAuthClient(
-            self.client_id,
-            self.client_secret,
-            self.username,
-            self.password,    
-        )
 
         #Defines the root, i.e. the window that opens when code is run
         self.root = tk.Tk()
@@ -33,6 +17,6 @@ class WorkspaceCheckerGUI:
     
     #Adds functionality to the button to retrieve and display the title of a hard-coded Podio item
     def fetch_info(self):
-        self.itemInfo = self.c.Item.find(2542371339)
+        self.itemInfo = c.Item.find(2542371339)
         self.displyItemInfo = tk.Label(self.root, text=f'Item name: {self.itemInfo["fields"][0]["values"][0]["value"]}', font=('Arial', 16))
         self.displyItemInfo.pack(padx=10, pady=10)
---
+++ backendProcesses.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('./src/pypodio2/')
+from pypodio2 import api
+
+#Credentials for Podio API authentication
+client_id = "olis-podio-app"
+client_secret = "BPuU6ZWR8uQP3HFMrU9HjfO1eEaChtJnDxoYosDEi8htMxakvzeVahCDnxzoXdm3"
+username = "oliver@theaccountancyfranchise.co.uk"
+password = "Oliver1234."
+
+#Authenticaiton with above credentials
+c = api.OAuthClient(
+    client_id,
+    client_secret,
+    username,
+    password,    
+)
+
