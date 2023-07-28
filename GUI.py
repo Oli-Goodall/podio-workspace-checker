abc03e4b5cb0c48ea2b9b17b1c73ea4af0c1b006
Oli-Goodall <oligoodall96@gmail.com>
2023-07-28 11:14:13 +0100

    Added scrollbar to autocomplete drop down

--- GUI.py
+++ GUI.py
@@ -6,6 +6,23 @@ class WorkspaceCheckerGUI:
 
     def __init__(self):
 
+        #Defines the root, i.e. the window that opens when code is run
+        self.root = tk.Tk()
+        self.root.geometry("500x300")
+
+        self.listboxFrame = tk.Frame(self.root)
+        self.listboxScrollbar = tk.Scrollbar(self.listboxFrame, orient=tk.VERTICAL)
+        self.franchiseeListBox = tk.Listbox(self.listboxFrame, width=20, height=10, yscrollcommand=self.listboxScrollbar.set)
+
+        
+        self.listboxScrollbar.config(command=self.franchiseeListBox.yview)
+        self.listboxScrollbar.config(command=self.franchiseeListBox.yview)
+
+        
+        #Hide listbox initially
+        self.franchiseeListBox.pack_forget()
+        self.listboxScrollbar.pack_forget()
+
         #Update the listbox
         def update(data):
             self.franchiseeListBox.delete(0, tk.END)
@@ -15,7 +32,7 @@ class WorkspaceCheckerGUI:
         #Puts selected item into input field
         def fillout(e):
             self.franchiseeSelectionInput.delete(0, tk.END)
-            self.franchiseeSelectionInput.insert(0, self.franchiseeListBox.get(tk.ACTIVE))
+            self.franchiseeSelectionInput.insert(0, self.franchiseeListBox.get(tk.ANCHOR))
 
         #Shows only results that contain the string in the input field
         def check(e):
@@ -30,21 +47,21 @@ class WorkspaceCheckerGUI:
                         data.append(item)
             
             update(data)
-
-        #Defines the root, i.e. the window that opens when code is run
-        self.root = tk.Tk()
-        self.root.geometry("500x300")
+            # Show the listbox only when there are matching results and the input field has focus
+            if data and self.franchiseeSelectionInput.focus_get() == self.franchiseeSelectionInput:
+                self.listboxFrame.pack()
+                self.franchiseeListBox.pack(side=tk.LEFT)
+                self.listboxScrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+            else:
+                self.franchiseeListBox.pack_forget()
+                self.listboxScrollbar.pack_forget()           
 
         #Displays an input field with placeholder
         self.franchiseeSelectionInput = entryWithPlaceholder.EntryWithPlaceholder(self.root, "Start typing location...")
         self.franchiseeSelectionInput.pack()
 
-        #Displays listbox populated with franchisee workspaces
-        self.franchiseeListBox = tk.Listbox(self.root)
-        self.franchiseeListBox.pack()
-
-        #Retireve list of franchisee space names from Podio
-        franchiseSpaceNames = list(space['name'] for space in backendProcesses.franchiseeWorkspacesList)
+        #Retrieve list of franchisee space names from Podio
+        franchiseSpaceNames = [space['name'].split('Accountants', 1)[-1].strip() for space in backendProcesses.franchiseeWorkspacesList]
 
         #Add franchisee space names to listbox
         update(franchiseSpaceNames)
@@ -54,7 +71,7 @@ class WorkspaceCheckerGUI:
 
         #Create a binding on the input field
         self.franchiseeSelectionInput.bind("<KeyRelease>", check)
-
+        
         #Tells the root window to render
         self.root.mainloop()
     
